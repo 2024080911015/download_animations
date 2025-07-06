721d617e96cd727645dcaff96451b2d731480fb8
2024080911015 <13271722058@163.com>
2025-07-06 20:16:26 +0800

    下载B站视频

--- download_bilibili_vedios.py
+++ download_bilibili_vedios.py
@@ -2,6 +2,7 @@ import requests
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 import asyncio
+import subprocess
 import time
 import aiofiles
 import aiohttp
@@ -12,7 +13,7 @@ from urllib3.util import url
 
 async def get_favorite_vedios(url:str):
     async with Stealth().use_async(async_playwright()) as p:
-        browser = await p.chromium.launch(headless=False)
+        browser = await p.chromium.launch(headless=True)
         content=await browser.new_context(storage_state="bilibili_cookies.json")
         page = await content.new_page()
         await page.goto(url,wait_until="networkidle")
@@ -39,13 +40,25 @@ async def get_favorite_vedios(url:str):
                 break
     return favorite_vedios
 
-async def download_bilibili_vedios(url:str):
-    pass
+def download_bilibili_vedios(url:str,output_dir:str):
+    command=[
+        "yt-dlp",
+        '-o',output_dir,
+        url
+    ]
+    subprocess.run(command)
+    print("成功下载"+str(url))
+
 async def main():
     url="https://space.bilibili.com/3546814376577843/favlist?fid=3427826043&ftype=create"
+    output_dir="F:/bilibili"
     vedios=await get_favorite_vedios(url)
     count=len(vedios)
     print(count)
+    for i,vedio in enumerate(vedios):
+        output_path=output_dir+"/"+str(i)+".mp4"
+        print("准备下载第"+str(i+1)+"个视频")
+        download_bilibili_vedios(vedio,output_path)
 if __name__ == "__main__":
     asyncio.run(main())
 
